8a9191f3c3ec5e5b2b2cebe423443c2181c374fb
Ambrósio <ambrosio@maistrovicz.com.br>
2012-11-17 11:45:07 -0200

    Commit test GitHub ... topo.php

---
+++ topo.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>Loja Virtual</title>
+</head>
+<?php include "connection/conexao.php";  //inclui o arquivo "conexao.php"
+?>
+<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" marginright="0">
+<center><img src="figuras/topo1.jpg" width="80%" height="11%"></center>
+<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr><th><table width="100%" border="0" align="center" cellpadding="2" cellspacing="0">
+      <tr>
+	    <a href="principal.php" target="_self"><img src="figuras/botao1.gif"
+		width="80" height="30" border="0"></a></th>
+		<th><div align="center"></div></th>
+		<a href="cadastro.php" target="_self"><img src="figuras/botao2.gif"
+		width="80" height="30" border="0"></a></th>
+		<th>&nbsp;</th>
+        <a href="menu_categorias.php" target="_self"><img src="figuras/botao3.gif"
+		width="80" height="30" border="0"></a></th>
+		<th>&nbsp;</th>
+		<a href="pedidos.php" target="_self"><img src="figuras/botao4.gif"
+		width="80" height="30" border="0"></a></th>
+		<th>&nbsp;</th>
+		<th>&nbsp;</th>
+		<th>&nbsp;</th>
+		<th>&nbsp;</th>
+       </tr>
+  </table></th></tr>
+</table>
+</body>
+</html>
